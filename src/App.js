dda02f297d2fd4ba2afd3d5501ba7d4e9c717854
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-07-19 16:33:31 +0700

    added navbar and made multiple components in sync

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import NavBar from "../src/components/navbar";
+import "./App.css";
+import Counters from "../src/components/counters";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 3 },
+      { id: 2, value: 4 },
+      { id: 3, value: 5 },
+      { id: 4, value: 6 }
+    ]
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(
+      counter => counter.id !== counterId
+    );
+    this.setState({ counters: counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map(counter => {
+      counter.value = 0;
+      return counter;
+    });
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    // update the state using a method inherited from Component
+    this.setState({ counters: counters });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <NavBar
+          totalCounters={
+            this.state.counters.filter(counter => counter.value > 0).length
+          }
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,53 +2,20 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 3 },
-      { id: 2, value: 4 },
-      { id: 3, value: 5 },
-      { id: 4, value: 6 }
-    ]
-  };
-
-  handleDelete = counterId => {
-    const counters = this.state.counters.filter(
-      counter => counter.id !== counterId
-    );
-    this.setState({ counters: counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map(counter => {
-      counter.value = 0;
-      return counter;
-    });
-    this.setState({ counters: counters });
-  };
-
-  handleIncrement = counter => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    // update the state using a method inherited from Component
-    this.setState({ counters: counters });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn btn-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map(counter => (
+        {this.props.counters.map(counter => (
           <Counter
             key={counter.id}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
           />
         ))}
