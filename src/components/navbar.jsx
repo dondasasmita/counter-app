74b58cd7c0500804beaeb71607029e63045a1e6e
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-07-19 17:29:57 +0700

    added stateless functional component as an alternative

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,19 @@
 import React, { Component } from "react";
 
+// Stateless Functional Component (An alternative for rendering a component)
+// const NavBar = () => {
+//   return (
+//     <nav className="navbar navbar-light bg-light">
+//       <a className="navbar-brand" href="#">
+//         Navbar{" "}
+//         <span className="badge badge-pill badge-secondary">
+//           {this.props.totalCounters}
+//         </span>
+//       </a>
+//     </nav>
+//   );
+// };
+
 class NavBar extends Component {
   render() {
     return (
