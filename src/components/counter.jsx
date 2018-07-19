e77c24a3b2fe85a3127ab1a8a5db37c08a05532a
dondasasmita@gmail.com <dondasasmita@gmail.com>
2018-07-19 08:56:19 +0700

    raised and handled delete event

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,10 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = () => {
-    // console.log(product);
     // update the state using a method inherited from Component
     this.setState({ value: this.state.value + 1 });
   };
@@ -20,6 +19,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
